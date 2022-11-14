32bc0e1bdf941229969425d6b9d06ce6f2cdf8ae
Ying Ying <yingyingfengg@gmail.com>
2022-11-13 21:40:15 -0500

    fix background issue on drinks

--- src/components/Drinks.js
+++ src/components/Drinks.js
@@ -6,16 +6,26 @@ const Drinks = ({ drinks }) => {
     return (
         <div>
             <Navbar />
-            <div className='drinks-banner'></div>
-            <h2>Drinks</h2>
-            <div>
-                
-            </div>
-            <div>
-               <p>Bear Milk Tea</p>
-               <p>Bear Fruit Tea</p>
-               <p>Bear Cap</p>
-               <p>Bear Slush</p>
+            <div className='drinks-main'>
+                <div className='drinks-banner'></div>
+                <div>
+                    <h1>Bear Milk Tea</h1>
+                    <p className='drinks-individual'>
+                        
+                    </p>
+                </div>
+                <div>
+                    <h1>Bear Fruit Tea</h1>
+                </div>
+                <div>
+                    <h1>Bear Matcha Latte</h1>
+                </div>
+                <div>
+                    <h1>Bear Speciality Drinks</h1>
+                </div>
+                <div>
+                    <h1>Bear Slush</h1>
+                </div>
             </div>
         </div>
     )
