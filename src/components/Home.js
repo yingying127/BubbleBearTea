7adafff091cf1d528a8e32c4f8640307f64a553c
Ying Ying <yingyingfengg@gmail.com>
2022-12-06 21:20:08 -0500

    home page change styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,22 +7,9 @@ const Home = ({}) => {
     return (
         <div className='banner'>
             <NavbarHome />
-            {/* <div className='navbar-home'>
-                <nav className='navbar'>
-                    <div className='navbar-logo'>
-                        <Link to="/home"> <img className='logo-home' src="/images/logo.png"/> </Link> 
-                    </div>
-                    <div className='navbar-main'>
-                        <li><Link to='/'>Home</Link></li>
-                        <li><Link to='/about'>About</Link></li>
-                        <li><Link to='/drinks'>Drinks</Link></li>
-                        <li><a href='https://www.toasttab.com/bubblebear-tea-160-walt-whitman-rd-1017/v3'>Order</a></li>
-                    </div>
-                </nav>
-            </div> */}
             <div>
                 <h1 className='home-h1'>Entering Bubble Bear Tea</h1>
-                <h3 className='home-h3'>Expanding the possibilities of tea. <br></br>Freshly brewed loose leaf tea paired with honey coated boba.</h3>
+                <h3 className='home-h3'>Expanding the possibilities of tea. <br></br>Fresh brewed loose leaf tea paired with honey coated boba.</h3>
             </div>
         </div>
     )
