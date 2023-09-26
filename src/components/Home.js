0719b7a5bce5f418debaabdd3323a568d11df89b
Ying Ying Feng <yingyingfengg@gmail.com>
2023-09-26 18:03:54 -0400

    working on home page and changing bottom nav

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,24 +5,30 @@ import NavbarHome from './NavbarHome.js'
 
 const Home = ({}) => {
     return (
-        <div className='banner'>
-            <NavbarHome />
-            {/* <div className='navbar-home'>
-                <nav className='navbar'>
-                    <div className='navbar-logo'>
-                        <Link to="/home"> <img className='logo-home' src="/images/logo.png"/> </Link> 
-                    </div>
-                    <div className='navbar-main'>
-                        <li><Link to='/'>Home</Link></li>
-                        <li><Link to='/about'>About</Link></li>
-                        <li><Link to='/drinks'>Drinks</Link></li>
-                        <li><a href='https://www.toasttab.com/bubblebear-tea-160-walt-whitman-rd-1017/v3'>Order</a></li>
-                    </div>
-                </nav>
-            </div> */}
-            <div>
-                <h1 className='home-h1'>Entering Bubble Bear Tea</h1>
-                <h3 className='home-h3'>Expanding the possibilities of tea. <br></br>Freshly brewed loose leaf tea paired with honey coated boba.</h3>
+        <div>
+            <div className='banner'>
+                <NavbarHome />
+                {/* <div className='navbar-home'>
+                    <nav className='navbar'>
+                        <div className='navbar-logo'>
+                            <Link to="/home"> <img className='logo-home' src="/images/logo.png"/> </Link> 
+                        </div>
+                        <div className='navbar-main'>
+                            <li><Link to='/'>Home</Link></li>
+                            <li><Link to='/about'>About</Link></li>
+                            <li><Link to='/drinks'>Drinks</Link></li>
+                            <li><a href='https://www.toasttab.com/bubblebear-tea-160-walt-whitman-rd-1017/v3'>Order</a></li>
+                        </div>
+                    </nav>
+                </div> */}
+                <div>
+                    <h1 className='home-h1'>Entering Bubble Bear Tea</h1>
+                    <h3 className='home-h3'>Expanding the possibilities of tea. <br></br>Freshly brewed loose leaf tea paired with honey coated boba.</h3>
+                </div>
+            </div>
+            <div className='banner-1'>
+                    <h2 className='banner-h'>Modern Bubble Tea Concept Coming Soon</h2>
+                    <p className='banner-p'>Join Us For Your Perfect Bubble Tea, Coffee, & Fresh Pastries</p>
             </div>
         </div>
     )
